3fa05f471b9362160c28127a2ee8e07f7a0588f3
akstarcoder <100538151+akstarcoder@users.noreply.github.com>
2023-06-28 14:01:46 +0530

    Assignment 3 Java codes Upload

---
+++ Assignment 3 - Arrays/CanAttend.java
@@ -0,0 +1,16 @@
+// Q.8 Given an array of meeting time intervals where intervals[i] = [starti, endi], determine if a person could attend all meetings.
+
+public class MeetingRooms {
+	public static boolean canAttend(int[][] intervals){
+		if(intervals == null || intervals.length == 0) return true;
+		for(int i = 1; i < intervals.length; i++){
+			if(intervals[i][0] < intervals[i - 1][1])
+				return false;
+		}
+		return true;
+	}
+	public static void main(String[] args) {
+		int[][] t = {{0, 3}, {5, 10}, {15, 20}};
+		System.out.println(canAttend(t));
+	}
+}
---
+++ Assignment 3 - Arrays/ThreeSumClosest.java
@@ -0,0 +1,33 @@
+//  Q.1 Given an integer array nums of length n and an integer target, find three integers
+// in nums such that the sum is closest to the target.
+// Return the sum of the three integers.
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        int minDiff=Integer.MAX_VALUE;
+        int ans=0;
+        for(int i=0;i<n-2;i++){
+            int low=i+1,high=n-1;
+            while(low<high){
+                int temp=nums[i]+nums[low]+nums[high];
+                if(Math.abs(target-temp)<minDiff){
+                    ans=temp;
+                    minDiff=Math.abs(target-temp);
+                }
+                if(temp==target){
+                    return target;
+                }
+                else if(temp>target){
+                        high--;
+                }
+                else{
+                    low++;
+                }
+            }
+        }
+        return ans;
+            
+    }       
+}
