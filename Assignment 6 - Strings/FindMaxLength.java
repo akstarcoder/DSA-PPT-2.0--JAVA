45f8541332e2a28d12e4dc2c46daa1ec4769cb3b
akstarcoder <100538151+akstarcoder@users.noreply.github.com>
2023-07-01 14:34:46 +0530

    Assignment 6

---
+++ Assignment 6 - Strings/FindMaxLength.java
@@ -0,0 +1,42 @@
+
+/*
+**Question 4**
+-------------------------
+
+Given a binary array nums, return *the maximum length of a contiguous subarray with an equal number of* 0 *and* 1.
+
+**Example 1:**
+
+**Input:** nums = [0,1]
+
+**Output:** 2
+
+**Explanation:**
+
+[0, 1] is the longest contiguous subarray with an equal number of 0 and 1.
+
+*/
+
+
+class Solution {
+    public int findMaxLength(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int zeros = 0, ones = 0;
+            for (int j = i; j < nums.length; j++) {
+                if (nums[j] == 0) {
+                    zeros++;
+                } else {
+                    ones++;
+                }
+                if (zeros == ones) {
+                    count = Math.max(count, j - i + 1);
+                }
+            }
+        }
+        return count;
+    }
+}
+
+
+
---
+++ Assignment 6 - Strings/Multiply.java
@@ -0,0 +1,44 @@
+
+/*
+
+**Question 8**
+-------------------------
+Given two [sparse matrices](https://en.wikipedia.org/wiki/Sparse_matrix) mat1 of size m x k and mat2 of size k x n, return the result of mat1 x mat2. You may assume that multiplication is always possible.
+
+**Example 1:**
+**Input:** mat1 = [[1,0,0],[-1,0,3]], mat2 = [[7,0,0],[0,0,0],[0,0,1]]
+
+**Output:**
+
+[[7,0,0],[-7,0,3]]
+*/
+
+
+class Solution {
+    public int[][] multiply(int[][] mat1, int[][] mat2) {
+        int r1 = mat1.length, c1 = mat1[0].length, c2 = mat2[0].length;
+        int[][] res = new int[r1][c2];
+        Map<Integer, List<Integer>> mp = new HashMap<>();
+        for (int i = 0; i < r1; ++i) {
+            for (int j = 0; j < c1; ++j) {
+                if (mat1[i][j] != 0) {
+                    mp.computeIfAbsent(i, k -> new ArrayList<>()).add(j);
+                }
+            }
+        }
+        for (int i = 0; i < r1; ++i) {
+            for (int j = 0; j < c2; ++j) {
+                if (mp.containsKey(i)) {
+                    for (int k : mp.get(i)) {
+                        res[i][j] += mat1[i][k] * mat2[k][j];
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
+
+
+
+
